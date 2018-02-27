7f509c478da44511ad0ec0c54bc8f7499a792b8a
Jim Barnes <jimbarnesdeveloper@gmail.com>
2018-02-27 13:30:02 -0500

    Bumped version number and updated readme

--- ucf-news.php
+++ ucf-news.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: UCF News
 Description: Contains shortcode and widget for displaying UCF News Feeds
-Version: 2.1.0
+Version: 2.1.1
 Author: UCF Web Communications
 License: GPL3
 */
