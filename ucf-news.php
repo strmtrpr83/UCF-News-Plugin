63da70e9f8b40b2facaef7238e51bb2a6f1179dc
Jim Barnes <jimbarnesdeveloper@gmail.com>
2018-02-27 11:37:25 -0500

    Configured defaults for plugin

--- ucf-news.php
+++ ucf-news.php
@@ -10,21 +10,37 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
+define( 'UCF_NEWS__PLUGIN_FILE', __FILE__ );
+
+require_once 'includes/ucf-news-config.php';
+require_once 'includes/ucf-news-feed.php';
+require_once 'includes/ucf-news-common.php';
+require_once 'includes/ucf-news-shortcode.php';
+require_once 'includes/ucf-news-widget.php';
+require_once 'admin/ucf-news-admin.php';
+require_once 'admin/ucf-news-api.php';
+
+require_once 'layouts/ucf-news-classic.php';
+require_once 'layouts/ucf-news-modern.php';
+require_once 'layouts/ucf-news-card.php';
+
+if ( ! function_exists( 'ucf_news_activate' ) ) {
+	function ucf_news_activate() {
+		UCF_News_Config::add_options();
+	}
+
+	register_activation_hook( UCF_NEWS__PLUGIN_FILE, 'ucf_news_activate' );
+}
+
+if ( ! function_exists( 'ucf_news_deactivate' ) ) {
+	function ucf_new_deactivate() {
+		UCF_News_Config::delete_options();
+	}
+
+	register_deactivation_hook( UCF_NEWS__PLUGIN_FILE, 'ucf_news_deactivate' );
+}
+
 add_action( 'plugins_loaded', function() {
-
-	define( 'UCF_NEWS__PLUGIN_FILE', __FILE__ );
-
-	require_once 'includes/ucf-news-config.php';
-	require_once 'includes/ucf-news-feed.php';
-	require_once 'includes/ucf-news-common.php';
-	require_once 'includes/ucf-news-shortcode.php';
-	require_once 'includes/ucf-news-widget.php';
-	require_once 'admin/ucf-news-admin.php';
-	require_once 'admin/ucf-news-api.php';
-
-	require_once 'layouts/ucf-news-classic.php';
-	require_once 'layouts/ucf-news-modern.php';
-	require_once 'layouts/ucf-news-card.php';
 
 	add_action( 'init', array( 'UCF_News_Shortcode', 'register_shortcode' ) );
 	add_action( 'admin_init', array( 'UCF_News_Shortcode', 'register_shortcode_interface' ) );
